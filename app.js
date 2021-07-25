f94f74ff3162ee28a7e844cf014b98e67b816e9b
nurvedin <nurvedin@hotmail.com>
2021-07-25 21:45:13 +0200

    splitting the string by capital letters and pipe character

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-/*
-const input = require("./string")
 
+let input = require("./string")
+/*
 console.log("")
 console.log(input.input)
 console.log("")
@@ -28,7 +28,7 @@ for (i = 0; i < x.length; i++) {
 //document.getElementById("demo").innerHTML = xmlDoc.getElementsByTagName("bookstore")
 */
 
-
+/*
 let doc = document.implementation.createDocument("", "", null)
 let peopleElem = doc.createElement("people")
 
@@ -140,6 +140,31 @@ personElem2.appendChild(addressElem3)
 doc.appendChild(peopleElem)
 
 console.log(doc)
+*/
+
+let text = "P|Carl Gustaf|Bernadotte"+
+"T|0768-101801|08-101801"+
+"A|Drottningholms slott|Stockholm|10001"+
+"F|Victoria|1977"+
+"A|Haga Slott|Stockholm|10002"+
+"F|Carl Philip|1979"+
+"T|0768-101802|08-101802"+
+"P|Barack|Obama"+
+"A|1600 Pennsylvania Avenue|Washington, D.C"
+
+let splittedString = text.split(/(?=[A-Z]\|)/g)
+
+//min pipe separator \|
+
+console.log(splittedString[1])
+//for(let i = 0; i < text.length; i++) {
+  //console.log(text[i].match(/[A-Z][a-z]+/g))
+  /*
+  if(text[i] == text[i].toUpperCase() && text[i+1] == "|") {
+    console.log("YES")
+  }
+  */
+//}
 
 /*
 <people>
